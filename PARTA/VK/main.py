29d9d71cfddc5905480274d385fd4a04a52c0f52
egorywka <88832718+egorbeckish@users.noreply.github.com>
2025-07-05 21:56:00 +0700

    Delete PARTA directory

--- PARTA/VK/main.py
+++
@@ -1,2 +0,0 @@
-from class_vk import *
-from class_db import *
--- PARTA/automessage/__init__.py
+++
@@ -1,7 +0,0 @@
-from main import *
-
-app.secret_key = ' '
-
-if __name__ == '__main__':
-	#webbrowser.open('http://127.0.0.1:5000/')
-	app.run(debug=True)
